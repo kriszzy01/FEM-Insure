e1a1d03a90605687bc408c7bda3f303fdebe3483
Christian Otu <christianotu.d@gmail.com>
2020-08-17 15:45:46 +0100

    Updates

---
+++ precache-manifest.0c8604ad6de40fce1a99d64e579782e5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f07a93e5fc7585afb038e41563d27d",
+    "url": "/FEM-Insure/index.html"
+  },
+  {
+    "revision": "1efb607fb799e3c33c40",
+    "url": "/FEM-Insure/static/css/main.3fcae40c.chunk.css"
+  },
+  {
+    "revision": "d185232850d7a140176e",
+    "url": "/FEM-Insure/static/js/2.b399add0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FEM-Insure/static/js/2.b399add0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efb607fb799e3c33c40",
+    "url": "/FEM-Insure/static/js/main.f54927e2.chunk.js"
+  },
+  {
+    "revision": "5d24530b8c12a0fbb1dc",
+    "url": "/FEM-Insure/static/js/runtime-main.446e76fc.js"
+  },
+  {
+    "revision": "562149a81a4a5d64fcb7309999bdbcf9",
+    "url": "/FEM-Insure/static/media/bg-pattern-intro-left-mobile.562149a8.svg"
+  },
+  {
+    "revision": "ef3d23bd373b877c0f109cf00fccdd0d",
+    "url": "/FEM-Insure/static/media/bg-pattern-intro-right-mobile.ef3d23bd.svg"
+  },
+  {
+    "revision": "09a91c1a381a6fc1ecb8512490b74a12",
+    "url": "/FEM-Insure/static/media/image-intro-mobile.09a91c1a.jpg"
+  },
+  {
+    "revision": "1bc452ac6f5856ef32a25e4519a4db91",
+    "url": "/FEM-Insure/static/media/logo.1bc452ac.svg"
+  },
+  {
+    "revision": "2922ed2c81c026d066328fbea874fefb",
+    "url": "/FEM-Insure/static/media/mobile-nav.2922ed2c.svg"
+  }
+]);
